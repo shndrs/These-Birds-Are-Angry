79f4f54268014c7d7bc4066cdeed89ce97ae4d6c
NP2 <sahandraeisi1994@gmail.com>
2019-05-21 11:33:59 +0430

    Prepare Projcet basics

--- These Birds Are Angry/GameViewController.swift
+++ These Birds Are Angry/GameViewController.swift
@@ -31,20 +31,4 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
